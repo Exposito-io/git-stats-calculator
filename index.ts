67de0ee051ef837b88114dbe850e53a5c5a9569d
Mathew Cormier <mathew.corm@gmail.com>
2017-06-15 22:26:39 -0400

    Add promise

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
 import * as git from 'simple-git'
 
 export async function gitStats(opts: GitStatsOptions) {
-    opts = Object.assign(new GitStatsOptions(), opts)
 
-    git(opts.dir).raw(
-    [
-        'config',
-        '--global',
-        'advice.pushNonFastForward',
-        'false'
-    ], (err, result) => {
-    
-        // err is null unless this command failed
-        // result is the raw output of this command
-    
+    return new Promise((resolve, reject) => {
+
+        opts = Object.assign(new GitStatsOptions(), opts)
+
+        git(opts.dir).raw(
+        [
+            'config',
+            '--global',
+            'advice.pushNonFastForward',
+            'false'
+        ], (err, result) => {
+        
+            // err is null unless this command failed
+            // result is the raw output of this command
+        
+        })
     })
 }
 
